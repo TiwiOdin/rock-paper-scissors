cbdff3b653841d808417178e4d8c0eb36268f88a
TiwiOdin <tiwiodindeveloper245@gmail.com>
2022-09-13 01:25:45 -0500

    console log -> dom elements

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 let userPoints = 0
 let computerPoints = 0
 
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+
+rockBtn.addEventListener('click', function(){pre_playRound("rock")});
+paperBtn.addEventListener('click', function(){pre_playRound("paper")});
+scissorsBtn.addEventListener('click', function(){pre_playRound("scissors")});
+
+const results = document.querySelector('.results');
+
+function pre_playRound(userChoice) {
+    let computerSelection = getComputerChoice();
+    playRound(userChoice, computerSelection);
+}
+
 function getUserChoice() {
     let userChoice = prompt("Rock, Paper or Scissor").toLowerCase()
     if(userChoice != "rock" && userChoice != "paper" && userChoice != "scissor") {
@@ -52,8 +67,18 @@ function playRound(playerSelection, computerSelection) {
         textRound = textRoundFunction(2, playerSelection, computerSelection)
     }
 
+
     console.log(`User Selection: ${playerSelection}`)
     console.log(`Computer Selection: ${computerSelection}`)
+    
+    const p1 = document.createElement('p');
+    p1.textContent = `User Selection: ${playerSelection}`;
+
+    const p2 = document.createElement('p');
+    p2.textContent = `Computer Selection: ${computerSelection}`
+
+    results.appendChild(p1);
+    results.appendChild(p2);
 
     return textRound
 }
@@ -78,3 +103,8 @@ function game() {
 }
 
 game()
+
+// When you don’t need a branch anymore it can be deleted using git branch -d <branch_name> 
+// if the branch has already been merged into main, or with git branch -D <branch_name> if it hasn’t. 
+// You will usually want to delete branches when you’re done with them, 
+// otherwise they can pile up and make it more difficult to find the branch you’re looking for when you need it.
